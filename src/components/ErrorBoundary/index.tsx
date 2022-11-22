a954dc31868aeef7ed07139aab2e6b16d41dfeb1
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-11-22 14:17:09 -0300

    feat(components): add ErrorBoundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,31 @@
+import { Component, ReactNode } from 'react'
+
+type Props = {
+  fallback: ReactNode
+  children: ReactNode
+}
+
+type State = {
+  hasError: boolean
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback
+    }
+
+    return this.props.children
+  }
+}
