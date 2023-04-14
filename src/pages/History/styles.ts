036acb85cd7d13a6117e19adf1e55a67ede4e691
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-04-14 14:07:28 -0300

    feat(HistoryPage): add structure and styles

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,146 @@
+import { keyframes, styled } from '@styles/stitches.config'
+
+export namespace HistoryPageStyles {
+  const fadeInAnimation = keyframes({
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: 1,
+    },
+  })
+
+  export const Container = styled('div', {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+
+    padding: '$large',
+    gap: '$medium',
+
+    '@media (prefers-reduced-motion: no-preference)': {
+      animation: `${fadeInAnimation} 150ms ease-out`,
+    },
+
+    '> h1': {
+      fontSize: '$headline',
+    },
+  })
+
+  export const History = styled('div', {
+    flex: 1,
+
+    overflow: 'auto',
+
+    '> div': { height: 0 },
+  })
+
+  export const Table = styled('table', {
+    width: '100%',
+    minWidth: '600px',
+
+    borderCollapse: 'collapse',
+
+    whiteSpace: 'nowrap',
+  })
+
+  export const TableHead = styled('thead', {
+    backgroundColor: '$defaultElementBackgroundActive',
+
+    position: 'sticky',
+    top: 0,
+
+    th: {
+      fontSize: '$normal',
+      lineHeight: '$normal',
+      textAlign: 'left',
+
+      padding: '$normal',
+
+      '&:first-child': {
+        borderTopLeftRadius: '$small',
+        paddingLeft: '$medium',
+      },
+
+      '&:last-child': {
+        borderTopRightRadius: '$small',
+        paddingRight: '$medium',
+      },
+    },
+  })
+
+  export const TableBody = styled('tbody', {
+    tr: {
+      backgroundColor: '$defaultElementBackground',
+
+      '&:last-child > td:first-child': {
+        borderBottomLeftRadius: '$small',
+      },
+
+      '&:last-child > td:last-child': {
+        borderBottomRightRadius: '$small',
+      },
+
+      '&:hover': {
+        backgroundColor: '$defaultElementBackgroundHover',
+      },
+    },
+
+    td: {
+      fontSize: '$normal',
+      lineHeight: '$normal',
+      textAlign: 'left',
+      color: '$defaultLowContrastText',
+
+      padding: '$normal',
+      borderTopWidth: '4px',
+      borderTopStyle: 'solid',
+      borderTopColor: '$defaultSubtleBackground',
+
+      '&:first-child': {
+        width: '50%',
+        paddingLeft: '$medium',
+      },
+
+      '&:last-child': {
+        paddingRight: '$medium',
+      },
+    },
+  })
+
+  export const Status = styled('span', {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '$small',
+
+    variants: {
+      statusColor: {
+        success: {
+          '&::before': {
+            backgroundColor: '$successSolid',
+          },
+        },
+        failure: {
+          '&::before': {
+            backgroundColor: '$dangerSolid',
+          },
+        },
+        inProgress: {
+          '&::before': {
+            backgroundColor: '$brandSolid',
+          },
+        },
+      },
+    },
+
+    '&::before': {
+      $$size: '8px',
+
+      content: '',
+
+      width: '$$size',
+      height: '$$size',
+      borderRadius: '$$size',
+    },
+  })
+}
