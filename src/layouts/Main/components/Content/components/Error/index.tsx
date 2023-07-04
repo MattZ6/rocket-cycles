86d8e9094123fae7c2f2ae6f425a8fe0f6c83bed
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-07-04 15:46:55 -0300

    feat(ErrorComponent): translate texts (#101)

--- src/layouts/Main/components/Content/components/Error/index.tsx
+++ src/layouts/Main/components/Content/components/Error/index.tsx
@@ -1,21 +1,24 @@
 import { ArrowClockwise, SmileyXEyes } from '@phosphor-icons/react'
+import { useTranslation } from 'react-i18next'
 
 import { ErrorStyles as Styles } from './styles'
 
 export function Error() {
+  const { t } = useTranslation('error')
+
   return (
     <Styles.Container>
       <SmileyXEyes size={96} weight="light" />
-      <Styles.Title>Ooh... Something went wrong!</Styles.Title>
+      <Styles.Title>{t('title')}</Styles.Title>
       <Styles.Message>
-        We&rsquo;re currently investigating the problem.
+        {t('description.first_line')}
         <br />
-        You can also refresh the page to try again.
+        {t('description.second_line')}
       </Styles.Message>
 
       <Styles.RetryButton onClick={() => window.location.reload()}>
         <ArrowClockwise size={20} weight="light" />
-        Refresh the page
+        {t('button')}
       </Styles.RetryButton>
     </Styles.Container>
   )
